dd34a356484b53de52a7f8a27a6f7a9600a6df30
Nilay <nilay.parmar@wickes.co.uk>
2025-09-16 19:08:10 +0530

    feat: add Graph's basic problems

---
+++ src/Graph/DetectCycleInUndirectedGraph.java
@@ -0,0 +1,49 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetectCycleInUndirectedGraph {
+    public static void main(String[] args) {
+        int V = 5;
+        int[][] edges = {{0, 1}, {0, 2}, {0, 3}, {1, 2}, {3, 4}};
+    }
+
+    public boolean detectCycle(int[][] edges, int n) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            int source = edge[0];
+            int dest = edge[1];
+
+            adj.get(source).add(dest);
+            adj.get(dest).add(source);
+        }
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                if (dfs(adj, visited, i, -1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(List<List<Integer>> adj, boolean[] visited, int node, int parent) {
+        visited[node] = true;
+        for (Integer neig : adj.get(node)) {
+            if (!visited[neig]) {
+                if (dfs(adj, visited, neig, node)) {
+                    return true;
+                }
+            } else if (parent != neig) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/Graph/TopologicalSort.java
@@ -0,0 +1,81 @@
+package Graph;
+
+import java.util.*;
+
+public class TopologicalSort {
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[][] edges = {{2, 3}, {3, 1}, {4, 0}, {4, 1}, {5, 0}, {5, 2}};
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] ints : edges) {
+            adj.get(ints[0]).add(ints[1]);
+        }
+        System.out.println(Arrays.toString(new TopologicalSort().topologicalSortDfs(adj, n)));
+        System.out.println(Arrays.toString(new TopologicalSort().topologicalSortBfs(adj, n)));
+    }
+
+    public int[] topologicalSortBfs(List<List<Integer>> adj, int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n];
+        int[] indegree = new int[n];
+
+        for (int u = 0; u < n; u++) {
+            for (int v : adj.get(u)) {
+                indegree[v]++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        List<Integer> topo = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            Integer node = queue.poll();
+            topo.add(node);
+            for (Integer nei : adj.get(node)) {
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    queue.add(nei);
+                }
+            }
+        }
+
+        return topo.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] topologicalSortDfs(List<List<Integer>> adj, int n) {
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(adj, stack, visited, i);
+            }
+        }
+
+        int[] result = new int[n];
+        int i = 0;
+        while (!stack.isEmpty()) {
+            result[i] = stack.pop();
+            i++;
+        }
+        return result;
+    }
+
+    public void dfs(List<List<Integer>> adj, Stack<Integer> stack, boolean[] visited, int node) {
+        visited[node] = true;
+        for (Integer nei : adj.get(node)) {
+            if (!visited[nei]) {
+                dfs(adj, stack, visited, nei);
+            }
+        }
+        stack.push(node);
+    }
+}
